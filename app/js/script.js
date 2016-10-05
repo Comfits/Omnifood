d036abc8cf36041c832b8b01c6f577954c19a088
Hoang Tuan <hoanganhtuan.ou@gmail.com>
2016-10-05 22:46:09 +0700

    Finish!!!

--- app/js/script.js
+++ app/js/script.js
@@ -15,4 +15,70 @@ $(document).ready(function(){
 		}
 		e.preventDefault();
 	});
+
+	$(function(){
+		$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+	$('.js-section-features').waypoint(function(direction){
+		if(direction == 'down')
+			$('nav').addClass('sticky');
+		else
+			$('nav').removeClass('sticky');
+	},{
+		offset: '60px'
+	});
+
+	$('.js-wp-1').waypoint(function(direction){
+		$('.js-wp-1').addClass('animated fadeIn');
+	},{
+		offset: '70%'
+	});
+	$('.js-wp-2').waypoint(function(direction){
+		$('.js-wp-2').addClass('animated slideInLeft');
+	},{
+		offset: '70%'
+	});
+	$('.js-wp-3').waypoint(function(direction){
+		$('.js-wp-3').addClass('animated jello');
+	},{
+		offset: '70%'
+	});
+
+	$('.logo-black').click(function(){
+		$('html,body').animate({scrollTop: 0},1000);
+	});
+
+	var latValue = 48.8709664,lngValue = 2.3597744;
+	var map = new GMaps({
+		div: '.map',
+		lat: latValue,
+		lng: lngValue,
+		zoom: 16,
+		scrollWheel: false
+	});
+	GMaps.geolocate({
+		success: function(position) {
+			map.setCenter(latValue, lngValue);
+  		map.addMarker({
+	  		lat: latValue,
+				lng: lngValue,
+				title: 'Omnifood\'s Office',
+				infoWindow: {
+					content: 'Omnifood\'s Offiice'
+  			}
+  		})
+  	}
+	});
 });
